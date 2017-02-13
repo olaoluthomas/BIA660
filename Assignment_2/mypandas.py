ad6e7da91cef5bbd9a913eb4cf1b581d40b2bca5
Simeon Thomas <thomasolaoluwa@gmail.com>
2017-02-13 03:31:25 -0500

    Big help from Zac completing the tasks and insight to the next HWK...

--- Assignment_2/mypandas.py
+++ Assignment_2/mypandas.py
@@ -1,5 +1,4 @@
 import csv
-import numpy
 
 from collections import OrderedDict, Counter
 from dateutil.parser import parse
@@ -23,8 +22,9 @@ class DataFrame(object):
             self.header = list_of_lists[0]
             self.data = list_of_lists[1:]
             self.iterable = None
+            self.row_of_rows = None
 
-            # If any headers are duplicated, raise TypeError
+            # If any headers are duplicated, raise an exception
             if len(Counter(self.header).items()) < len(self.header):
                 raise Exception('You have duplicate headers in your data')
 
@@ -96,86 +96,81 @@ class DataFrame(object):
             raise TypeError('You must pass a header of <type str> to retrieve data with column names')
 
     def min(self, column_name):
-        def isfloat(x):
-            if type(float(x)) == float:
-                return True
-            else:
-                return False
-
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [row[column_name].replace(',', '') for row in self.data]
-                if [isfloat(self.iterable[string])is True for string in self.iterable]:
-                    self.iterable = [float(string) for string in self.iterable]
-                else:
-                    self.iterable = [parse(string) for string in self.iterable]
-                return min(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
-        else:
-            raise TypeError('You must pass a header of <type str> to retrieve data with column names')
+        self.iterable = [row[column_name].replace(',', '') for row in self.data]
+        return min([float(string) if isfloat(string) else stringToDatetime(string) for string in self.iterable])
 
     def max(self, column_name):
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [float(row[column_name].replace(',', '')) for row in self.data]
-                return max(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
-        else:
-            raise TypeError('You must pass a header of <type str> to retrieve data with column names')
+        self.iterable = [row[column_name].replace(',', '') for row in self.data]
+        return max([float(string) if isfloat(string) else stringToDatetime(string) for string in self.iterable])
 
     def sum(self, column_name):
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [float(row[column_name].replace(',', '')) for row in self.data]
-                return sum(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
-        else:
-            raise TypeError('You must pass a header of <type str> to retrieve data with column names')
+        self.iterable = [row[column_name].replace(',', '') for row in self.data]
+        return sum([float(string) if isfloat(string) else stringToDatetime(string) for string in self.iterable])
 
     def mean(self, column_name):
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [float(row[column_name].replace(',', '')) for row in self.data]
-                return numpy.average(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
-        else:
-            raise TypeError('You must pass a header of <type str> to retrieve data with column names')
+        return self.sum(column_name)/len(self[column_name])
 
     def median(self, column_name):
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [float(row[column_name].replace(',', '')) for row in self.data]
-                return numpy.median(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
+        self.iterable = sorted([row[column_name].replace(',', '') for row in self.data])
+        self.iterable = [float(string) if isfloat(string) else stringToDatetime(string) for string in self.iterable]
+        midd = len(self.iterable)/2
+        if len(self.iterable) % 2 == 0:
+            return (self.iterable[midd - 1] + self.iterable[midd]) / 2
         else:
-            return TypeError('You must pass a header of <type str> to retrieve data with column names')
+            return self.iterable[midd]
 
     def std(self, column_name):
-        if isinstance(column_name, str):
-            try:
-                self.iterable = [float(row[column_name].replace(',', '')) for row in self.data]
-                return numpy.std(self.iterable)
-            except:
-                raise TypeError('The data type in selected column is not numeric')
-        else:
-            return TypeError('You must pass a header of <type str> to retrieve data with column names')
+        mean = self.mean(column_name)
+        return (sum([(convertDataType(item) - mean)**2 for item in self[column_name]])/len(self[column_name]))**0.5
 
+    def add_rows(self, row_of_rows):
+        if all([len(row) == len(self.header) for row in row_of_rows]):
+            self.data.append([OrderedDict(zip(self.header, row)) for row in row_of_rows])
+        else:
+            raise Exception("Your data does not match the expected format")
+
+    def add_column(self, new_col, column_name):
+        if len(new_col) == len(self.data):
+            self.header.append(column_name)
+            for index, value in enumerate(new_col):
+                self.data[index][column_name] = value
+        else:
+            raise Exception("Your data doesn't match!!!")
+
+def convertDataType(string):
+    string_no_comma = string.replace(',', '')
+    if isfloat(string_no_comma):
+        return float(string_no_comma)
+    else:
+        return stringToDatetime(string)
+
+def isfloat(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
+
+def stringToDatetime(string):
+    try:
+        return parse(string)
+    except:
+        raise Exception('Invalid data type should be a float or timestamp string')
+
+# sorted(test, key=lambda x: x[1], reverse=True)
 
 df = DataFrame.from_csv('SalesJan2009.csv')
 get_col = df.get_column('Payment_Type')
 mns = df.min('Price')
-# mina = df.min('Transaction_date')
+mina = df.min('Transaction_date')
+# minBad = df.min('Payment_Type')
 mxs = df.max('Price')
 sums = df.sum('Price')
 means = df.mean('Price')
-medians = df.median('Price')
+# medians = df.median('Payment_Type')
 stddev = df.std('Price')
 
+adding = df.add_rows([[value for value in df[1].itervalues()]])
 # to test get_col
 # get_col2 = df.get_column(2)
 
