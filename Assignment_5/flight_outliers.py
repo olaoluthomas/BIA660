5b511a6691f4552cc5644caf7707f2befa24c0ed
olaoluthomas <thomasolaoluwa@gmail.com>
2017-04-15 22:59:37 -0400

    string and unicode conflict in cleaning data (clean_data list) & URL rectification done

--- Assignment_5/flight_outliers.py
+++ Assignment_5/flight_outliers.py
@@ -1,3 +1,4 @@
+import bs4
 import time
 import pandas as pd
 import datetime
@@ -6,6 +7,7 @@ from unidecode import unidecode
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from urlparse import urlparse, urlunparse
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
@@ -17,14 +19,15 @@ plt.style.use('ggplot')
 def scrape_data(start_date, from_place, to_place, city_name):
     date = str(start_date).split(' ')[0]
     driver = webdriver.Chrome()
-    driver.get('https://www.google.com/flights/explore/#explore;li=3;lx=5;d=' + date)
+    url = 'https://www.google.com/flights/explore/#explore;li=3;lx=5;d=' + date
+    driver.get(url)
     from_input = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[3]/div/div[2]/div')
     from_input.click()
     action1 = ActionChains(driver)
     action1.send_keys(from_place)
     action1.send_keys(Keys.ENTER)
     action1.perform()
-    time.sleep(0.05)
+    time.sleep(0.5)
 
     to_input = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[3]/div/div[4]/div/div')
     to_input.click()
@@ -32,6 +35,15 @@ def scrape_data(start_date, from_place, to_place, city_name):
     action2.send_keys(to_place)
     action2.send_keys(Keys.ENTER)
     action2.perform()
+    time.sleep(2)
+
+    url = driver.current_url
+    parsed_url = urlparse(url)
+    # url.replace(url[url.find('d='):], 'd={}'.format(date))
+    for param in parsed_url.fragment.split(';'):
+        if 'd=' in param:
+            url = url.replace(param, 'd={}'.format(date))
+    driver.get(url)
     time.sleep(5)
 
     # if the script breaks at this point, increase the amount of sleep time above to allow the element to stabilize
@@ -52,9 +64,10 @@ def scrape_data(start_date, from_place, to_place, city_name):
     clean_data = []
     for d in data:
         try:
-            clean_data.append([(parse(d[1].split('-')[0].strip()) - date).days, float(d[0].replace('$', '').replace(',', ''))])
-        except:
-            continue
+            clean_data.append(
+                [(parse(d[1].split('-')[0].strip()) - start_date).days, float(d[0].replace('$', '').replace(',', ''))])
+        except Exception as e:
+            print "Something's broken..."
     df = pd.DataFrame(clean_data, columns=['Day', 'Price'])
     return df
 
@@ -62,7 +75,7 @@ def scrape_data(start_date, from_place, to_place, city_name):
 def scrape_data_90(start_date, from_place, to_place, city_name):
     date = str(start_date).split(" ")[0]
     driver = webdriver.Chrome()
-    driver.get('https://www.google.com/flights/explore/#explore;li=3;lx=5;d=' + str(date))
+    driver.get('https://www.google.com/flights/explore/#explore;li=3;lx=5;d=' + date)
     from_input = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[3]/div/div[2]/div')
     from_input.click()
     action1 = ActionChains(driver)
@@ -78,6 +91,14 @@ def scrape_data_90(start_date, from_place, to_place, city_name):
     action2.send_keys(Keys.ENTER)
     action2.perform()
     time.sleep(2)
+
+    url = driver.current_url
+    parsed_url = urlparse(url)
+    for param in parsed_url.fragment.split(';'):
+        if 'd=' in param:
+            url = url.replace(param, 'd={}'.format(date))
+    driver.get(url)
+    time.sleep(5)
 
     # if the script breaks at this point, increase the amount of sleep time above to allow the element to stabilize
     results = driver.find_elements_by_class_name('LJTSM3-v-d')
@@ -118,7 +139,7 @@ def scrape_data_90(start_date, from_place, to_place, city_name):
     for d in data:
         try:
             clean_data.append(
-                [(parse(d[1].split('-')[0].strip()) - date).days, float(d[0].replace('$', '').replace(',', ''))])
+                [(parse(d[1].split('-')[0].strip()) - start_date).days, float(d[0].replace('$', '').replace(',', ''))])
         except:
             continue
     df = pd.DataFrame(clean_data, columns=['Day_of_Flight', 'Price'])
@@ -162,15 +183,15 @@ def task_3_dbscan(d_frame):
             outlier_lst.append((x, x_cluster))
 
     # populating the main clusters as lists of arrays belonging to each label so we can get the sd of each cluster
-    '''agg_of_clusters = []
-    for n in [num for num in range(lbls[-1] + 1)]:
-        locals()['cluster_{0}'.format(n)] = [x for x in X[db.labels_ == n]]
-        agg_of_clusters.append(locals()['cluster_{0}'.format(n)])'''
+    # agg_of_clusters = []
+    # for n in [num for num in range(lbls[-1] + 1)]:
+        # locals()['cluster_{0}'.format(n)] = [x for x in X[db.labels_ == n]]
+        # agg_of_clusters.append(locals()['cluster_{0}'.format(n)])
 
-    '''for outlier in outlier_lst:
-        if outlier[0][1] < np.mean([x[1] for x in agg_of_clusters[int(outlier[1])]]) \
-                - (2 * np.std([x[1] for x in agg_of_clusters[int(outlier[1])]])):
-            pass'''
+    for outlier in outlier_lst:
+        if outlier[0][1] < np.mean([x[1] for x in X[db.labels_ == outlier[1]]]) \
+                - (2 * np.std([x[1] for x in X[db.labels_ == outlier[1]]])):
+            pass
 
 def task_3_IQR(df_frame):
     sorted_frame = df_frame.sort_values(by='Price').reset_index()
